e42f4a78a4c21e3dd7c02e30372c6985bd77d3a8
eshwarsundar <northeasteditsofficial@gmail.com>
2022-03-07 23:51:39 +0530

    Add GameUser object model

---
+++ GameLibrary/GameUser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace GameLibrary {
+    class GameUser
+    {
+        private static string[] userNames;
+        public string UserName {
+            get
+            {
+                return this.UserName;
+            }
+            set
+            {
+                if (isUserExists(value))
+                {
+                    throw new Exception("User already exists");
+                }
+            }
+        }
+        private string _email;
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                if (!value.Contains("@"))
+                {
+                    throw new Exception("Invalid email");
+                }
+                else
+                {
+                    _email = value;
+                }
+            }
+        }
+        public string Id {
+            get
+            {
+                return UserName.GetHashCode().ToString();
+            }
+        }
+        public static bool isUserExists(string givenUser)
+        {
+            foreach (var user in GameUser.userNames)
+            {
+                if (user.Equals(givenUser))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    
+}
